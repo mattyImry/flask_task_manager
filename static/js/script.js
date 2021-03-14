90e82274bb9f2fd31e7bd9aa5cea24291c69bc70
mattyImry <matteo.imeri@gmail.com>
2021-03-14 16:23:52 +0000

    Add tasks from input field

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 5,
+        showClearBtn: true,
+        i18n : {
+            done: "Select"
+        }
+    })
   });
 

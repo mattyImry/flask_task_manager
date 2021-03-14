eec22f5d9bdf262c06453dfa098d1d71d7211ba7
mattyImry <matteo.imeri@gmail.com>
2021-03-14 14:48:08 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+    $('.tooltipped').tooltip();
   });
 
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-  });
